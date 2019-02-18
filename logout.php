23e5ebfda7dc79830fc58879d1d3e448f28b9500
Jason <jasontokarski@yahoo.com>
2019-02-18 16:39:47 -0600

    Formatting Change

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-unset($_SESSION);
-session_destroy();
-header("Location: index.php");
-echo "You have successfully logged out!";
+  session_start();
+  unset($_SESSION);
+  session_destroy();
+  header("Location: index.php");
+  echo "You have successfully logged out!";
 ?>
